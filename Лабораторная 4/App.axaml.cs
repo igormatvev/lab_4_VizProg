bed21a0c7112eba32c1596ef9387d9b32393fa2b
igormatvev <78473335+igormatvev@users.noreply.github.com>
2022-05-10 11:28:38 +0700

    Add files via upload

---
+++ Лабораторная 4/App.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Лабораторная_4.ViewModels;
+using Лабораторная_4.Views;
+
+namespace Лабораторная_4
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ Лабораторная 4/Models/RomanNumberException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RomanNumberException_
+{
+    public class RomanNumberException : Exception
+    {
+        public short Value { get; }
+        public RomanNumberException(string message) : base(message)
+        {
+
+        }
+    }
+}
